a5221091d93224beae60f6d3b911d14b64b8ff21
MelissaChater <melissachater58@gmail.com>
2025-02-10 08:26:11 +0000

    debugging pwa

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const appFiles = [
     'style.css',
     'sw.js',
     'favicon.ico',
+    'favicon.png',
     'vocabolario.json',
     'manifest.json',
     'README.md',
@@ -51,35 +52,45 @@ const appFiles = [
 
 ];
 
+// Caches all the PWA shell files (appFiles array) when the app is launched
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     const filesUpdate = cache => {
         const stack = [];
         appFiles.forEach(file => stack.push(
-            cache.add(file).catch(_ => console.error(`can't load ${file} to cache`))
+            cache.add(file).catch(_=>console.error(`can't load ${file} to cache`))
         ));
         return Promise.all(stack);
     };
     e.waitUntil(caches.open(cacheName).then(filesUpdate));
-});
-
-self.addEventListener('fetch', (e) => {
+  });
+  
+  // Called when the app fetches a resource like an image, caches it automatically except for pwaversion.txt, which is always fetched
+  self.addEventListener('fetch', (e) => {
     e.respondWith(
-        (async () => {
+      (async () => {
+        if(e.request.url.contains("pwaversion.txt")){
+            console.log(`[Service Worker] Fetching version info: ${e.request.url}`);
+            const response = await fetch(e.request);
+            return response;
+        }else{
             const r = await caches.match(e.request);
             console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
             if (r) {
-                return r;
+              return r;
             }
             const response = await fetch(e.request);
             const cache = await caches.open(cacheName);
             console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
             cache.put(e.request, response.clone());
             return response;
-        })()
+        }
+      })()
     );
-});
-
-self.addEventListener('activate', (e) => {
-    console.log("[Service Worker] Activated");
-});
+  });
+  
+  // Called when the service worker is started
+  self.addEventListener('activate', (e) => {
+      console.log("[Service Worker] Activated");
+  });
+  

68b12834ad9f1cf7d53ad89450adc2040c4b5d93
Riccardo Compagno <168272196+IlCompa@users.noreply.github.com>
2025-02-13 07:27:51 +0000

    refactor appendi function

--- script.js
+++ script.js
@@ -110,30 +110,30 @@ const predictWebcam = async () => {
 };
 
 // Function to handle appending text
-// const appendi = (result_text) => {
-//     const gestureOutput = document.getElementById("gesture_output");
-//     if (!result_text) {
-//         return;
-//     }
+const appendi = (result_text) => {
+    const gestureOutput = document.getElementById("gesture_output");
+    if (!result_text) {
+        return;
+    }
 
-//     if (result_text === ultimo_valore) {
-//         // Do nothing if the same character is entered twice in a row
-//     } else if (result_text === "del") {
-//         daStampare = daStampare.slice(0, -1); // Delete the last character
-//         gestureOutput.innerText = daStampare;
-//         ultimo_valore = "del";
-//     } else if (result_text === "not" || result_text === "None") {
-//         ultimo_valore = ""; // Reset the count
-//     } else if (result_text === "space") {
-//         daStampare += " ";
-//         gestureOutput.innerText = daStampare;
-//         ultimo_valore = "space"; // Reset the count
-//     } else {
-//         daStampare += result_text;
-//         ultimo_valore = result_text;
-//         gestureOutput.innerText = daStampare;
-//     }
-// };
+    if (result_text === ultimo_valore) {
+        // Do nothing if the same character is entered twice in a row
+    } else if (result_text === "del") {
+        daStampare = daStampare.slice(0, -1); // Delete the last character
+        gestureOutput.innerText = daStampare;
+        ultimo_valore = "del";
+    } else if (result_text === "not" || result_text === "None") {
+        ultimo_valore = ""; // Reset the count
+    } else if (result_text === "space") {
+        daStampare += " ";
+        gestureOutput.innerText = daStampare;
+        ultimo_valore = "space"; // Reset the count
+    } else {
+        daStampare += result_text;
+        ultimo_valore = result_text;
+        gestureOutput.innerText = daStampare;
+    }
+};
 
 document.addEventListener('DOMContentLoaded', async () => {
     const visionLibUrl = "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
